5bc6bdf667c020300e66fc3444e262d05c3b740a
Zack Smith <acidprime@wallcity.org>
2014-09-01 01:55:13 -0700

    Update

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,11 @@
 class unifi {
   apt::source { 'precise_ubiquiti':
     comment           => 'This is the ubiquiti precise 12.0.4 source',
-    location          => 'http://www.ubnt.com/downloads/unifi/distros/deb/precise',
+    location          => 'http://www.ubnt.com/downloads/unifi/distros/deb/precise/',
     repos             => 'ubiquiti',
     key               => 'C0A52C50',
     key_server        => 'keyserver.ubuntu.com',
+    include_src       => false,
   }
 
   package { 'unifi':
