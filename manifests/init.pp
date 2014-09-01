36b348076063772f5ec479bff1fdc9f7ebc1a3bb
Zack Smith <acidprime@wallcity.org>
2014-09-01 00:59:39 -0700

    Initial Commit

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+class unifi {
+  apt::source { 'precise_ubiquiti':
+    comment           => 'This is the ubiquiti precise 12.0.4 source',
+    location          => 'http://www.ubnt.com/downloads/unifi/distros/deb/precise',
+    repos             => 'main',
+    key               => 'C0A52C50',
+    key_server        => 'keyserver.ubuntu.com',
+  }
+
+  package { 'unifi':
+    ensure  => 'installed',
+    require => Apt::Source['precise_ubiquiti'],
+  }
+
+  class { 'mongodb::globals':
+    manage_package_repo => true,
+  }
+
+  class { 'mongodb':
+    enable_10gen => true,
+  }
+}
