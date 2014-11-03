5a52b315266cf6fe8a910ce1fe9bac4eaa423130
Zack Smith <zack@puppetlabs.com>
2014-11-02 18:10:21 -0800

    Update to use dpkg source

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,33 @@
 class unifi(
-  $version = '3.2.7-2347'
+  $version     = '3.2.7-2347',
+  $manage_repo = true,
 ){
-  apt::source { 'precise_ubiquiti':
-    comment           => 'This is the ubiquiti precise 12.0.4 source',
-    location          => 'http://www.ubnt.com/downloads/unifi/distros/deb/precise/',
-    repos             => 'ubiquiti',
-    key               => 'C0A52C50',
-    key_server        => 'keyserver.ubuntu.com',
-    include_src       => false,
+
+
+  if $manage_repo {
+    # Install from their repo
+    apt::source { 'precise_ubiquiti':
+      comment           => 'This is the ubiquiti precise 12.0.4 source',
+      location          => 'http://www.ubnt.com/downloads/unifi/distros/deb/precise/',
+      repos             => 'ubiquiti',
+      key               => 'C0A52C50',
+      key_server        => 'keyserver.ubuntu.com',
+      include_src       => false,
+    }
+    package { 'unifi':
+      ensure  => $version,
+      require => [ Apt::Source['precise_ubiquiti'], Class['mongodb::server'] ],
+    }
+  } else {
+    # Install from their downloads page
+    package { 'unifi':
+      ensure   => 'installed',
+      require  => Class['mongodb::server'],
+      source   => "http://www.ubnt.com/downloads/unifi/${version}/unifi_sysvinit_all.deb",
+      provider => 'dpkg',
+    }
   }
 
-  package { 'unifi':
-    ensure  => $version,
-    require => [ Apt::Source['precise_ubiquiti'], Class['mongodb::server'] ],
-  }
 
   service { 'unifi':
     ensure  => 'running',
